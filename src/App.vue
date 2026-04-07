d7f760d7600f886e7ea91e4a5b2a8d03a6e89c32
Elyabe <elyabebat@gmail.com>
2026-04-07 13:55:31 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import MenuForm from './components/MenuForm.vue'
+import MenuFilters from './components/MenuFilters.vue'
+import MenuItem from './components/MenuItem.vue'
+
+// Estado principal
+const itens = ref([])
+const filtroAtual = ref('Todas')
+
+// Métodos
+const adicionarItem = (novoItem) => {
+  itens.value.push({ ...novoItem, id: Date.now() })
+}
+const removerItem = (id) => {
+  itens.value = itens.value.filter(item => item.id !== id)
+}
+</script>
+
+<template>
+
+</template>
